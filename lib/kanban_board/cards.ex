fab9c306cd6d94b14f417b457b8ee3ebb8c7cbb7
EJ Llovido <ericllovido08@gmail.com>
2024-07-15 13:34:34 +0800

    Adds context

---
+++ lib/kanban_board/cards.ex
@@ -0,0 +1,30 @@
+defmodule KanbanBoard.Cards do
+  import Ecto.Query, warn: false
+
+  alias KanbanBoard.Card
+  alias KanbanBoard.Repo
+
+  def list_cards do
+    Repo.all(Card)
+  end
+
+  def get_card(id) do
+    Repo.get(Card, id)
+  end
+
+  def create_card(attrs) do
+    %Card{}
+    |> Card.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_card(card, attrs) do
+    card
+    |> Card.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_card(card) do
+    Repo.delete(card)
+  end
+end
---
+++ lib/kanban_board/categories.ex
@@ -0,0 +1,30 @@
+defmodule KanbanBoard.Categories do
+  import Ecto.Query, warn: false
+
+  alias KanbanBoard.Category
+  alias KanbanBoard.Repo
+
+  def list_categories do
+    Repo.all(Category)
+  end
+
+  def get_category(id) do
+    Repo.get(Category, id)
+  end
+
+  def create_category(attrs) do
+    %Category{}
+    |> Category.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_category(category, attrs) do
+    category
+    |> Category.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_category(category) do
+    Repo.delete(category)
+  end
+end
